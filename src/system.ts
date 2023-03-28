e40c2f0793de0086fd5581c9468477947cfe3ff8
dashsoap <360062589@qq.com>
2023-03-28 14:55:11 +0800

    wip

--- src/system.ts
+++ src/system.ts
@@ -8,7 +8,7 @@ export const setting = {
 }
 
 export const message = `Powered by OpenAI Vercel
-- 学会如何拥有自己的chatGPT，可以[加入我的星球](https://cdn.jsdelivr.net/gh/dashsoap/Chat/assets/pay.png)，不仅有教程提示，还有行业内各种动态更新，但不接受任何付费功能请求
+- 学会如何拥有自己的chatGPT，可以[加入我的星球](https://t.zsxq.com/0ckONzbJ0)，不仅有教程提示，还有行业内各种动态更新，但不接受任何付费功能请求
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
