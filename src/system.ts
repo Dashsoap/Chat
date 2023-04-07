6623cf5b99aed335db3c34cf0b4d1fc047714594
dashsoap <360062589@qq.com>
2023-04-07 17:14:02 +0800

    feat

--- src/system.ts
+++ src/system.ts
@@ -8,7 +8,7 @@ export const setting = {
 }
 
 export const message = `Powered by OpenAI Vercel
-- 维护费用过高，怀疑被刷API，因此设置密码。[加入我的星球](https://t.zsxq.com/0ckONzbJ0)，获取密码
+- 维护费用过高，怀疑被刷API，因此设置密码。[加入我的星球](https://t.zsxq.com/0ckONzbJ0)，获取密码，亲朋好友请微信联系我
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
